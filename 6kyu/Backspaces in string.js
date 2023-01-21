57cc402e7e1889fb2048b0484ec61b4d484d1304
VladenCode <vladimir.cvejanov@gmail.com>
2023-01-21 12:34:12 +0100

    Adding explanation to the Backspaces in string challenge

--- 6kyu/Backspaces in string.js
+++ 6kyu/Backspaces in string.js
@@ -26,3 +26,10 @@ function cleanString(s) {
 
 clean_string = (s) =>
   s.split('').reduce((r, e) => (e == '#' ? r.slice(0, -1) : r + e), '');
+
+//[?]
+/*
+1. Creates an array of characters with split() method
+2. Uses the reduce method to reduce the array of characters to one string(no need to join() method in the end) - concatenating the characters
+3. Using ternary operator check if the current(e) character is a '#' or not. If so, using the slice() method it starts from the 0 index of the string and because of the -1 (goes to the back of the string) it slices of the last element of the string. If the current element (e) is not equal to '#', then it concatenates the character to the string. 
+*/
